a293c1e007d3f94241331d577ef71c969d640ee4
j.parker <jparker@jasonparker.info>
2025-12-31 04:40:05 +0000

    2025 day 4 part two correct expected result

--- day04/y2025_test.go
+++ day04/y2025_test.go
@@ -27,18 +27,18 @@ func TestDay4B(t *testing.T) {
 
 func TestDay4C(t *testing.T) {
 	d := NewData(Example, Year2025)
-	
+
 	expected := 43
 	actual := CountRemovable(d)
-	
+
 	assert.Equal(t, expected, actual, MsgExpected, expected, actual)
 }
 
 func TestDay4D(t *testing.T) {
 	d := NewData(Challenge, Year2025)
-	
-	expected := -1
+
+	expected := 9086
 	actual := CountRemovable(d)
-	
+
 	assert.Equal(t, expected, actual, MsgExpected, expected, actual)
 }
