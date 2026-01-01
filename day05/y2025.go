706102607598fca500b9dcfe5abc4be6b97cd2e8
j.parker <jparker@jasonparker.info>
2026-01-01 00:24:06 +0000

    2025 day 5 final solution

--- day05/y2025.go
+++ day05/y2025.go
@@ -1,6 +1,7 @@
 package day05
 
 import (
+	"sort"
 	"strconv"
 	"strings"
 
@@ -12,8 +13,8 @@ type db struct {
 	ids    []int64
 }
 
-func day42025Transformer(lines []string) any {
-	db := &db{
+func DataTransformer(lines []string) any {
+	d := &db{
 		ranges: [][]int64{},
 		ids:    []int64{},
 	}
@@ -24,32 +25,63 @@ func day42025Transformer(lines []string) any {
 			foundDemarcation = true
 			continue
 		}
-
 		if foundDemarcation {
 			n, _ := strconv.ParseInt(l, 10, 64)
-			db.ids = append(db.ids, n)
+			d.ids = append(d.ids, n)
 		} else {
 			tokens := strings.Split(l, "-")
 			left, _ := strconv.ParseInt(tokens[0], 10, 64)
 			right, _ := strconv.ParseInt(tokens[1], 10, 64)
-			ranges := []int64{left, right}
-			db.ranges = append(db.ranges, ranges)
+			d.ranges = append(d.ranges, []int64{left, right})
 		}
 	}
-	return db
+	return d
+}
+
+func MergeIntervals(d *db) *db {
+	// Sort the ranges by their start value.
+	sort.Slice(d.ranges, func(i, j int) bool {
+		return d.ranges[i][0] < d.ranges[j][0]
+	})
+
+	merged := [][]int64{d.ranges[0]}
+	for i := 1; i < len(d.ranges); i++ {
+		last := merged[len(merged)-1]
+		current := d.ranges[i]
+
+		if current[0] <= last[1]+1 { // overlapping or contiguous
+			// Extend the end of the last interval if needed.
+			if current[1] > last[1] {
+				merged[len(merged)-1][1] = current[1]
+			}
+		} else {
+			merged = append(merged, current)
+		}
+	}
+
+	d.ranges = merged
+	return d
+}
+
+func NumberOfFreshRangeIds(d *utils.Data) int64 {
+	data := MergeIntervals(d.TransformData(DataTransformer).(*db))
+	total := int64(0)
+	for _, r := range data.ranges {
+		total += int64((r[1] - r[0]) + 1)
+	}
+	return total
 }
 
 func CountFreshIds(d *utils.Data) int {
-	db := d.TransformData(day42025Transformer).(*db)
-
+	data := d.TransformData(DataTransformer).(*db)
+	
 	count := 0
-	for _, id := range db.ids {
-		for _, r := range db.ranges {
-			if id < r[0] || id > r[1] {
-				continue
+	for _, id := range data.ids {
+		for _, r := range data.ranges {
+			if id >= r[0] && id <= r[1] {
+				count++
+				break
 			}
-			count++
-			break
 		}
 	}
 	return count
--- day05/y2025_test.go
+++ day05/y2025_test.go
@@ -24,3 +24,21 @@ func Test2025Day5B(t *testing.T) {
 	
 	assert.Equal(t, expected, actual, utils.MsgExpected, expected, actual)
 }
+
+func Test2025Day5C(t *testing.T) {
+	d := utils.NewData(utils.Example, utils.Year2025)
+	
+	var expected int64 = 14
+	actual := NumberOfFreshRangeIds(d)
+	
+	assert.Equal(t, expected, actual, utils.MsgExpected, expected, actual)
+}
+
+func Test2025Day5D(t *testing.T) {
+	d := utils.NewData(utils.Challenge, utils.Year2025)
+	
+	var expected int64 = 346240317247002
+	actual := NumberOfFreshRangeIds(d)
+	
+	assert.Equal(t, expected, actual, utils.MsgExpected, expected, actual)
+}
