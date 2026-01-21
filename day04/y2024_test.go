179479d3d3b224337c15575968ce95a852976097
j.parker <jparker@jasonparker.info>
2026-01-21 20:40:22 +0000

    Initialize test data with error handling in each test function
    The changes update the test functions to initialize the test data using
    `utils.NewData()` with error handling instead of relying on global
    variables. This ensures that each test function properly handles
    potential errors when loading the test data.

--- day04/y2024_test.go
+++ day04/y2024_test.go
@@ -7,21 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var ex2024 = utils.NewData(utils.Example, utils.Year2024)
-var ch2024 = utils.NewData(utils.Challenge, utils.Year2024)
-
 func Test2024A(t *testing.T) {
+	ex2024, err := utils.NewData(utils.Example, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 18, Count("xmas", ex2024))
 }
 
 func Test2024B(t *testing.T) {
+	ch2024, err := utils.NewData(utils.Challenge, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 2504, Count("xmas", ch2024))
 }
 
 func Test2024C(t *testing.T) {
+	ex2024, err := utils.NewData(utils.Example, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 9, CountPattern("mas", ex2024, SearchXPattern))
 }
 
 func Test2024D(t *testing.T) {
+	ch2024, err := utils.NewData(utils.Challenge, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 1923, CountPattern("mas", ch2024, SearchXPattern))
 }
--- day04/y2025_test.go
+++ day04/y2025_test.go
@@ -7,21 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var ex2025 = utils.NewData(utils.Example, utils.Year2025)
-var ch2025 = utils.NewData(utils.Challenge, utils.Year2025)
-
 func Test2025A(t *testing.T) {
+	ex2025, err := utils.NewData(utils.Example, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 13, CountAccessible(ex2025))
 }
 
 func Test2025B(t *testing.T) {
+	ch2025, err := utils.NewData(utils.Challenge, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 1416, CountAccessible(ch2025))
 }
 
 func Test2025C(t *testing.T) {
+	ex2025, err := utils.NewData(utils.Example, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 43, CountRemovable(ex2025))
 }
 
 func Test2025D(t *testing.T) {
+	ch2025, err := utils.NewData(utils.Challenge, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 9086, CountRemovable(ch2025))
 }
