07276238bda9266805847d53de6a2b43d37d7a69
j.parker <jparker@jasonparker.info>
2026-01-21 20:42:11 +0000

    Initialize test data with error handling in each test function
    This change updates the test functions to initialize the test data using
    utils.NewData with error handling instead of using global variables.
    This ensures that each test function properly handles potential errors
    when loading the test data, making the tests more robust and explicit
    about their dependencies.

--- day09/y2024_test.go
+++ day09/y2024_test.go
@@ -7,21 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var ex2024 = utils.NewData(utils.Example, utils.Year2024)
-var ch2024 = utils.NewData(utils.Challenge, utils.Year2024)
-
 func Test2024A(t *testing.T) {
+	ex2024, err := utils.NewData(utils.Example, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 1928, ChecksumHarddrive(ex2024, CompactNormal))
 }
 
 func Test2024B(t *testing.T) {
+	ch2024, err := utils.NewData(utils.Challenge, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 6262891638328, ChecksumHarddrive(ch2024, CompactNormal))
 }
 
 func Test2024C(t *testing.T) {
+	ex2024, err := utils.NewData(utils.Example, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 2858, ChecksumHarddrive(ex2024, CompactLeft))
 }
 
 func Test2024D(t *testing.T) {
+	ch2024, err := utils.NewData(utils.Challenge, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 6287317016845, ChecksumHarddrive(ch2024, CompactLeft))
 }
--- day09/y2025_test.go
+++ day09/y2025_test.go
@@ -7,21 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var exampleData = utils.NewData(utils.Example, utils.Year2025)
-var challengeData = utils.NewData(utils.Challenge, utils.Year2025)
-
 func Test2025A(t *testing.T) {
+	exampleData, err := utils.NewData(utils.Example, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 50, findLargestRectangle(exampleData))
 }
 
 func Test2025B(t *testing.T) {
+	challengeData, err := utils.NewData(utils.Challenge, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 4756718172, findLargestRectangle(challengeData))
 }
 
 func Test2025C(t *testing.T) {
+	exampleData, err := utils.NewData(utils.Example, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 24, findLargestRectangleOfAny(exampleData))
 }
 
 func Test2025D(t *testing.T) {
+	challengeData, err := utils.NewData(utils.Challenge, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 1665679194, findLargestRectangleOfAny(challengeData))
 }
