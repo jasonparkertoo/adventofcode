b67841e4a5093ee0fa947e66160ef7067f42e53f
j.parker <jparker@jasonparker.info>
2026-01-21 20:42:03 +0000

    Move data loading into test functions
    This change refactors the test files to initialize the example and
    challenge data
    within each test function rather than at the package level. This ensures
    that
    each test has its own data instance and removes the global variable
    declarations.

--- day08/y2024_test.go
+++ day08/y2024_test.go
@@ -7,21 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var ex2024 = utils.NewData(utils.Example, utils.Year2024)
-var ch2024 = utils.NewData(utils.Challenge, utils.Year2024)
-
 func Test2024A(t *testing.T) {
+	ex2024, err := utils.NewData(utils.Example, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 14, CountUniqueLocations(ex2024))
 }
 
 func Test2024B(t *testing.T) {
+	ch2024, err := utils.NewData(utils.Challenge, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 261, CountUniqueLocations(ch2024))
 }
 
 func Test2024C(t *testing.T) {
+	ex2024, err := utils.NewData(utils.Example, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 34, CountUniqueLocationsHarmonics(ex2024))
 }
 
 func Test2024D(t *testing.T) {
+	ch2024, err := utils.NewData(utils.Challenge, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 988, CountUniqueLocationsHarmonics(ch2024))
 }
--- day08/y2025_test.go
+++ day08/y2025_test.go
@@ -7,21 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var ex2025 = utils.NewData(utils.Example, utils.Year2025)
-var ch2025 = utils.NewData(utils.Challenge, utils.Year2025)
-
 func Test2025A(t *testing.T) {
+	ex2025, err := utils.NewData(utils.Example, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 40, ProductOfThreeLargestCircuits(ex2025, 10))
 }
 
 func Test2025B(t *testing.T) {
+	ch2025, err := utils.NewData(utils.Challenge, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 330786, ProductOfThreeLargestCircuits(ch2025, 1000))
 }
 
 func Test2025C(t *testing.T) {
+	ex2025, err := utils.NewData(utils.Example, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 25272, ProductOfLastConnectionX(ex2025))
 }
 
 func Test2025D(t *testing.T) {
+	ch2025, err := utils.NewData(utils.Challenge, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 3276581616, ProductOfLastConnectionX(ch2025))
 }
