eed048afcfadcfb4761097754c0f3976e82070b5
j.parker <jparker@jasonparker.info>
2026-01-21 20:42:50 +0000

    Move data initialization into test functions
    The test functions now initialize the data directly instead of using
    package-level variables. This improves test isolation and makes the
    tests more explicit about their data dependencies.

--- day12/y2024_test.go
+++ day12/y2024_test.go
@@ -7,13 +7,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var ex2024 = utils.NewData(utils.Example, utils.Year2024)
-var ch2024 = utils.NewData(utils.Challenge, utils.Year2024)
-
 func TestDay12A(t *testing.T) {
+	ex2024, err := utils.NewData(utils.Example, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 1930, CalculateTotalPrice(ex2024))
 }
 
 func TestDay12B(t *testing.T) {
+	ch2024, err := utils.NewData(utils.Challenge, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 1451030, CalculateTotalPrice(ch2024))
 }
