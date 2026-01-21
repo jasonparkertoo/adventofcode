2015c2ff603cabf24f3a1baab9c68d1bec4275cb
j.parker <jparker@jasonparker.info>
2026-01-21 20:40:10 +0000

    Initialize test data with error handling
    The test functions now initialize their data using utils.NewData and
    handle potential errors with assert.NoError. This replaces the previous
    approach of declaring data variables at the package level.

--- day03/y2024_test.go
+++ day03/y2024_test.go
@@ -7,17 +7,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var ex2024 = utils.NewData(utils.Example, utils.Year2024)
-var ch2024 = utils.NewData(utils.Challenge, utils.Year2024)
-
 func Test2024A(t *testing.T) {
+	ex2024, err := utils.NewData(utils.Example, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 161, CalculateUncorrupted(ex2024))
 }
 
 func Test2024B(t *testing.T) {
+	ch2024, err := utils.NewData(utils.Challenge, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 156388521, CalculateUncorrupted(ch2024))
 }
 
 func Test2024C(t *testing.T) {
+	ch2024, err := utils.NewData(utils.Challenge, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 75920122, Calculate(ch2024))
 }
--- day03/y2025_test.go
+++ day03/y2025_test.go
@@ -7,21 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var ex2025 = utils.NewData(utils.Example, utils.Year2025)
-var ch2025 = utils.NewData(utils.Challenge, utils.Year2025)
-
 func Test2025A(t *testing.T) {
+	ex2025, err := utils.NewData(utils.Example, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, int64(357), TotalOutputJoltage(ex2025, 2))
 }
 
 func Test2025B(t *testing.T) {
+	ch2025, err := utils.NewData(utils.Challenge, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, int64(17376), TotalOutputJoltage(ch2025, 2))
 }
 
 func Test2025C(t *testing.T) {
+	ex2025, err := utils.NewData(utils.Example, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, int64(3121910778619), TotalOutputJoltage(ex2025, 12))
 }
 
 func Test2025D(t *testing.T) {
+	ch2025, err := utils.NewData(utils.Challenge, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, int64(172119830406258), TotalOutputJoltage(ch2025, 12))
 }
