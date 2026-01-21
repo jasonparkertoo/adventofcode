3759d9240d17b9998346328f89db70272a7a607d
j.parker <jparker@jasonparker.info>
2026-01-21 20:42:18 +0000

    Move data initialization into test functions
    Initialize test data directly in test functions instead of at package
    level to avoid potential issues with shared state between tests.

--- day10/y2024_test.go
+++ day10/y2024_test.go
@@ -7,21 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var ex2024 = utils.NewData(utils.Example, utils.Year2024)
-var ch2024 = utils.NewData(utils.Challenge, utils.Year2024)
-
 func TestDay10A(t *testing.T) {
+	ex2024, err := utils.NewData(utils.Example, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 36, TotalTrailheadScore(ex2024))
 }
 
 func TestDay10B(t *testing.T) {
+	ch2024, err := utils.NewData(utils.Challenge, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 776, TotalTrailheadScore(ch2024))
 }
 
 func TestDay10C(t *testing.T) {
+	ex2024, err := utils.NewData(utils.Example, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 81, TotalTrailheadRating(ex2024))
 }
 
 func TestDay10D(t *testing.T) {
+	ch2024, err := utils.NewData(utils.Challenge, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 1657, TotalTrailheadRating(ch2024))
 }
--- day10/y2025_test.go
+++ day10/y2025_test.go
@@ -7,28 +7,33 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var ex2025 = utils.NewData(utils.Example, utils.Year2025)
-var ch2025 = utils.NewData(utils.Challenge, utils.Year2025)
-
 func Test2025A(t *testing.T) {
+	ex2025, err := utils.NewData(utils.Example, utils.Year2025)
+	assert.NoError(t, err)
 	expected := 7
 	actual, _ := FewestButtonPresses(ex2025)
 	assert.Equal(t, expected, actual)
 }
 
 func Test2025B(t *testing.T) {
+	ch2025, err := utils.NewData(utils.Challenge, utils.Year2025)
+	assert.NoError(t, err)
 	expected := 404
 	actual, _ := FewestButtonPresses(ch2025)
 	assert.Equal(t, expected, actual)
 }
 
 func Test2025C(t *testing.T) {
+	ex2025, err := utils.NewData(utils.Example, utils.Year2025)
+	assert.NoError(t, err)
 	expected := 33
 	actual, _ := CalculateScore(ex2025)
 	assert.Equal(t, expected, actual)
 }
 
 func Test2025D(t *testing.T) {
+	ch2025, err := utils.NewData(utils.Challenge, utils.Year2025)
+	assert.NoError(t, err)
 	expected := 16474
 	actual, _ := CalculateScore(ch2025)
 	assert.Equal(t, expected, actual)
