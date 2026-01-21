e410ce0b5a0409283f15a7783c44e055e30232b4
j.parker <jparker@jasonparker.info>
2026-01-21 20:39:44 +0000

    Move test data initialization into individual test functions
    This change refactors the test files to initialize test data directly
    within each test function rather than at the package level. This
    improves test isolation and makes it clearer which data is being used
    for each specific test case.

--- day02/y2024_test.go
+++ day02/y2024_test.go
@@ -7,21 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var ex2024 = utils.NewData(utils.Example, utils.Year2024)
-var ch2024 = utils.NewData(utils.Challenge, utils.Year2024)
-
 func Test2024A(t *testing.T) {
+	ex2024, err := utils.NewData(utils.Example, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 2, NumberOfSafeReports(false, ex2024))
 }
 
 func Test2024B(t *testing.T) {
+	ch2024, err := utils.NewData(utils.Challenge, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 606, NumberOfSafeReports(false, ch2024))
 }
 
 func Test2024C(t *testing.T) {
+	ex2024, err := utils.NewData(utils.Example, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 4, NumberOfSafeReports(true, ex2024))
 }
 
 func Test2024D(t *testing.T) {
+	ch2024, err := utils.NewData(utils.Challenge, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 644, NumberOfSafeReports(true, ch2024))
 }
--- day02/y2025_test.go
+++ day02/y2025_test.go
@@ -7,28 +7,33 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var ex2025 =  utils.NewData(utils.Example, utils.Year2025)
-var ch2025 =  utils.NewData(utils.Challenge, utils.Year2025)
-
 func TestDay2A(t *testing.T) {
+	ex2025, err := utils.NewData(utils.Example, utils.Year2025)
+	assert.NoError(t, err)
 	expected := 1227775554
 	actual, _ := SumInvalidIds(ex2025)
 	assert.Equal(t, expected, actual)
 }
 
 func TestDay2B(t *testing.T) {
+	ch2025, err := utils.NewData(utils.Challenge, utils.Year2025)
+	assert.NoError(t, err)
 	expected := 15873079081
 	actual, _ := SumInvalidIds(ch2025)
 	assert.Equal(t, expected, actual)
 }
 
 func TestDay2C(t *testing.T) {
+	ex2025, err := utils.NewData(utils.Example, utils.Year2025)
+	assert.NoError(t, err)
 	expected := 4174379265
 	_, actual := SumInvalidIds(ex2025)
 	assert.Equal(t, expected, actual)
 }
 
 func TestDay2D(t *testing.T) {
+	ch2025, err := utils.NewData(utils.Challenge, utils.Year2025)
+	assert.NoError(t, err)
 	expected := 22617871034
 	_, actual := SumInvalidIds(ch2025)
 	assert.Equal(t, expected, actual)
