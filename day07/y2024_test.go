057446625721da0c8bb88d7324cdba242f07ef4e
j.parker <jparker@jasonparker.info>
2026-01-21 20:41:55 +0000

    Initialize test data with error handling in 2024 and 2025 tests

--- day07/y2024_test.go
+++ day07/y2024_test.go
@@ -7,21 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var ex2024 = utils.NewData(utils.Example, utils.Year2024)
-var ch2024 = utils.NewData(utils.Challenge, utils.Year2024)
-
 func Test2024A(t *testing.T) {
+	ex2024, err := utils.NewData(utils.Example, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 3749, TotalCalibrationResult(ex2024))
 }
 
 func Test2024B(t *testing.T) {
+	ch2024, err := utils.NewData(utils.Challenge, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 3598800864292, TotalCalibrationResult(ch2024))
 }
 
 func Test2024C(t *testing.T) {
+	ex2024, err := utils.NewData(utils.Example, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 11387, TotalCalibrationResultWithConcat(ex2024))
 }
 
 func Test2024D(t *testing.T) {
+	ch2024, err := utils.NewData(utils.Challenge, utils.Year2024)
+	assert.NoError(t, err)
 	assert.Equal(t, 340362529351427, TotalCalibrationResultWithConcat(ch2024))
 }
--- day07/y2025_test.go
+++ day07/y2025_test.go
@@ -7,21 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var ex2025 = utils.NewData(utils.Example, utils.Year2025)
-var ch2025 = utils.NewData(utils.Challenge, utils.Year2025)
-
 func Test2025A(t *testing.T) {
+	ex2025, err := utils.NewData(utils.Example, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 21, CountBeamSplits(ex2025))
 }
 
 func Test2025B(t *testing.T) {
+	ch2025, err := utils.NewData(utils.Challenge, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 1533, CountBeamSplits(ch2025))
 }
 
 func Test2025C(t *testing.T) {
+	ex2025, err := utils.NewData(utils.Example, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 40, NumberOfTachyonParticleTimelines(ex2025))
 }
 
 func Test2025D(t *testing.T) {
+	ch2025, err := utils.NewData(utils.Challenge, utils.Year2025)
+	assert.NoError(t, err)
 	assert.Equal(t, 10733529153890, NumberOfTachyonParticleTimelines(ch2025))
 }
